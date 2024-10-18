b2dc4476e89edac770b381f9903af47df4697b3b
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-10-18 16:30:03 +0530

    chnages added

---
+++ src/pages/Servicefour.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Servicefour = () => {
+  return (
+    <div>
+      <h1>hellofour</h1>
+    </div>
+  )
+}
+
+export default Servicefour
---
+++ src/pages/Serviceone.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Serviceone = () => {
+  return (
+    <div>
+      <h1>hello1</h1>
+    </div>
+  )
+}
+
+export default Serviceone
